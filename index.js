d5beae7140170a2fbc2caeea6c81549e3db0d078
ade detumbi <adetumbi@faycal.com>
2021-07-16 00:49:16 -0400

    changed my mind

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Dojo!\n'
+  const msg = 'Hallo Hallo!\n'
   res.end(msg);
 });
 
